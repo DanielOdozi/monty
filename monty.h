64d044ce85c4395a4f9542e58e1092cddd5cb696
root <root@DanielOdozi>
2023-10-18 14:54:54 +0100

    np

--- monty.h
+++ monty.h
@@ -41,4 +41,4 @@ int is_number(const char *str);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
-#endif /*MONTY_H*/
+#endif /*MAIN_H*/
