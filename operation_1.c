3c3f4e7f66b6838f49dde41a4ff5cf6db4956218
root <root@DanielOdozi>
2023-10-18 12:31:46 +0100

    Push and Pall

---
+++ operation_1.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+stack_t *stack = NULL;
+
+void push(stack_t **stack) {
+    stack_t *new_node;
+    char *value = strtok(NULL, " \n");
+    if (value == NULL || !is_number(value)) {
+        fprintf(stderr, "Usage: push integer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = atoi(value);
+    new_node->prev = NULL;
+    new_node->next = *stack;
+    if (*stack != NULL) {
+        (*stack)->prev = new_node;
+    }
+    *stack = new_node;
+}
+
+void pall(stack_t **stack) {
+    stack_t *current = *stack;
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
+int is_number(const char *str) {
+    if (*str == '-')
+        str++;
+    for (; *str; str++) {
+        if (*str < '0' || *str > '9')
+            return 0;
+    }
+    return 1;
+}
+
+int main(void) {
+    char opcode[256];
+    char *line = NULL;
+
+    while (fgets(opcode, sizeof(opcode), stdin) != NULL) {
+        if (strcmp(opcode, "push\n") == 0) {
+            push(&stack);
+        } else if (strcmp(opcode, "pall\n") == 0) {
+            pall(&stack);
+        } else {
+            fprintf(stderr, "Unknown instruction: %s", opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    free(line);
+    while (stack != NULL) {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    return 0;
+}
